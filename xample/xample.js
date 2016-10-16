6a09f26dad44307a46840b60e624dd009701e440
picokol <serpicofossas@gmail.com>
2016-10-16 13:22:00 -0600

    added node clusters

--- xample/xample.js
+++ xample/xample.js
@@ -61,20 +61,21 @@ switch(host) {
 
 // <<<code>>>
 
-/// can't add clusters until sessions can be stored persistently & accessed across clusters
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-// if (cluster.isMaster) {
-// 	/* Fork workers */
-// 	for (var i = 0; i < numCPUs; i++) {
-// 		cluster.fork();
-// 	}
-//
-// 	cluster.on('exit', (worker, code, signal) => {
-// 		console.log(`worker ${worker.process.pid} died`);
-// 	});
-// } else {
+if (cluster.isMaster) {
+	/* fork workers */
+	for (var i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	}
+
+	/* if a cluster dies, fork a new one */
+	cluster.on('exit',function(worker,code,signal) {
+		console.log(`Worker Died > process-pid: ${worker.process.pid}, code: ${code}, signal: ${signal}`);
+		cluster.fork();
+	});
+} else {
 
 // <<<fold>>>
 
@@ -309,4 +310,4 @@ app.listen(port,function() {
 
 // <<<fold>>>
 
-//} // end parentheses for else{} of cluster
+} // end parentheses for else{} of cluster
